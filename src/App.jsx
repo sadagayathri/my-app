1656172702a7dfad8f0de23ac28fabf0edb770f5
sadagayathri <gayathrisada022@gmail.com>
2023-08-28 12:45:29 +0530

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter'
-import Todolist from './Todolist';
+import Todolist from './Todolist'
 
 function App() {
   return (
